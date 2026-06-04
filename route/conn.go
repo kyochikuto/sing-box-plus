b2bedd45f6068539a364cdaece29dc45a9f90843
Kyochikuto | キョウチクトウ <kyochikuto@proton.me>
2026-06-04 14:07:34 +0330

    feat(tls): rebase TLS fragmentation on the Xray-Core implementation

--- route/conn.go
+++ route/conn.go
@@ -14,7 +14,7 @@ import (
 	"github.com/sagernet/sing-box/adapter"
 	"github.com/sagernet/sing-box/common/dialer"
 	"github.com/sagernet/sing-box/common/sniff"
-	"github.com/sagernet/sing-box/common/tlsfragment"
+	tf "github.com/sagernet/sing-box/common/tlsfragment"
 	C "github.com/sagernet/sing-box/constant"
 	"github.com/sagernet/sing/common"
 	"github.com/sagernet/sing/common/buf"
@@ -126,8 +126,8 @@ func (m *ConnectionManager) NewConnection(ctx context.Context, this N.Dialer, co
 		m.logger.ErrorContext(ctx, err)
 		return
 	}
-	if metadata.TLSFragment || metadata.TLSRecordFragment {
-		remoteConn = tf.NewConn(remoteConn, ctx, metadata.TLSFragment, metadata.TLSRecordFragment, metadata.TLSFragmentFallbackDelay)
+	if metadata.TLSFragment.Enabled {
+		remoteConn = tf.NewConn(ctx, remoteConn, metadata.TLSFragment.Packets, metadata.TLSFragment.Length, metadata.TLSFragment.Interval, metadata.TLSFragment.MaxSplits)
 	}
 	serverFirst := sniff.Skip(&metadata)
 	var done atomic.Bool
